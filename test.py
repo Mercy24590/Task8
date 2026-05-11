b22c0683ab5e7eaed0a4b5fd514cc81b86b84e62
songbupt <ssong21@sina.com>
2026-05-12 09:45:05 +1200

    connecting python and database

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+'''Mercy Song -- Library catalog database application.'''
 import sqlite3
 
+#Storing the database to a variable
 DATABASE = 'Library_Catalog.db'
+
+#Connecting python to my database
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = 'SELECT * FROM books;'
+cursor.execute(sql)
+results = cursor.fetchall()
+#loop through all the results
+print('book_name                     author                   genre               publish_year')
+for book in results:
+    print(f'{book[1]:<30}{book[2]:<25}{book[3]:<20}{book[4]:<20}')
+    #loop finished here
+db.close()
