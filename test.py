43f6453d358e8e86c06863ee025bb1241e205e43
songbupt <ssong21@sina.com>
2026-05-19 14:40:11 +1200

    adding more to the while loop

--- test.py
+++ test.py
@@ -75,10 +75,12 @@ def print_all_books_sort_by_genre():
         #loop finished here
     db.close()
 
-'''def add_books():
+def add_books():
     #adding more books to the library database
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    bookname = input ('Enter your book name: ')
+    author = input('Enter the author: ')
     sql = 'SELECT * FROM books;'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -87,7 +89,7 @@ def print_all_books_sort_by_genre():
     for book in results:
         print(f'{book[1]:<30}{book[2]:<25}{book[3]:<20}{book[4]:<20}')
         #loop finished here
-    db.close()'''
+    db.close()
 
 # functions for borrowers table
 def print_all_borrowers():
@@ -98,45 +100,118 @@ def print_all_borrowers():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print('borrower_id                    author                   genre               publish_year')
+    print('fist_name                     last_name                email')
     for person in results:
-        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}{person[4]:<20}')
+        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}')
         #loop finished here
     db.close()
 
-def print_all_borrowers():
-    '''Show all borrowers details'''   
+def print_all_borrowers_sort_by_first_names():
+    '''Show all borrowers details and order them by their first names'''   
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = 'SELECT * FROM borrowers;'
+    sql = 'SELECT * FROM borrowers ORDER BY first_name;'
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print('borrower_id                    author                   genre               publish_year')
+    print('fist_name                     last_name                email')
     for person in results:
-        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}{person[4]:<20}')
+        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}')
         #loop finished here
     db.close()
-    
+
+def print_all_borrowers_sort_by_last_names():
+    '''Show all borrowers details and order them by their last names'''   
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM borrowers ORDER BY last_name;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print('fist_name                     last_name                email')
+    for person in results:
+        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}')
+        #loop finished here
+    db.close()
+
+def print_all_borrowers_sort_by_emails():
+    '''Show all borrowers details and order them by their emails'''   
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM borrowers ORDER BY email;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print('fist_name                     last_name                email')
+    for person in results:
+        print(f'{person[1]:<30}{person[2]:<25}{person[3]:<20}')
+        #loop finished here
+    db.close()
+
+
+ #creating a while loop for the user to use the functions. 
 while True:
+# asking what the user want to do.
     user_input = input(
 """
-What would you like to do.
+What would you like to do?
+a. See details about books
+b. See details about the borrowers
+c. See details about recent loans
+d. Exit\n""")
+    
+    #If the user wants to see details about the books, ask if they want to do
+    if user_input == 'a':
+        user_input_for_books = input(
+"""
+What would you like to do?
 1. Print all books
 2. Print all books sort by book names
 3. Print all books sort by authors
 4. Print all books sort by genre
 5. Print all books sort by published years
-6. Print all books sort by 
-7. Exit\n""")
-    if user_input == '1':
-        print_all_books()
-    if user_input == '2':
-        print_all_books_sort_by_book_name()
-    if user_input == '3':
-        print_all_books_sort_by_author()
-    if user_input == '4':
-        print_all_books_sort_by_genre()
+6. Exit\n""")
+        #Call different functions when different numbers are inputed
+        if user_input_for_books == '1':
+            print_all_books()
+        elif user_input_for_books == '2':
+            print_all_books_sort_by_book_name()
+        elif user_input_for_books == '3':
+            print_all_books_sort_by_author()
+        elif user_input_for_books == '4':
+            print_all_books_sort_by_genre()
+        elif user_input_for_books == '5':
+            print_all_books_by_sort_publish_year()
+        elif user_input_for_books == '6':
+            break
+        else:
+            print('That is not an option.\n')
 
+
+    #If the user wants to see details about the books, ask if they want to do
+    if user_input == 'b':
+        user_input_for_borrowers = input(
+"""
+What would you like to do?
+1. Print all borrower details
+2. Print all borrower details order by first names
+3. Print all borrower details order by last names
+4. Print all borrower details order by emails
+5. Exit\n""")
+        if user_input_for_borrowers == '1':
+            print_all_borrowers()
+        elif user_input_for_borrowers == '2':
+            print_all_borrowers_sort_by_first_names()
+        elif user_input_for_borrowers == '3':
+            print_all_borrowers_sort_by_last_names()
+        elif user_input_for_borrowers == '4':
+            print_all_borrowers_sort_by_emails()
+        elif user_input_for_borrowers == '5':
+            break
+        else:
+            print('That is not an option.\n')
+
+    if user_input == 'd':
+        break
 
 
